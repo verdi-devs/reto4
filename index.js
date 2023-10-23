dfd354cc09f7067345858cf82df9e366c40630fd
verdi-devs <verdi.devs@gmail.com>
2023-10-23 12:26:58 -0400

    update

--- index.js
+++ index.js
@@ -1,10 +1,6 @@
-/* alert("Este es un mensaje de alerta"); */
-// Obtén una referencia al botón y al elemento que deseas cambiar
-const botonCambiarEstilo = document.getElementById('cambiarEstilo');
-const elementoAfectado = document.getElementById('elementoAfectado');
+const botonCambiarEstilo = document.getElementById('botonCambiarEstilo');
 
-// Agrega un manejador de eventos al botón para el evento de clic
-botonCambiarEstilo.addEventListener('click', () => {
-    // Agrega o quita una clase CSS al elemento
-    elementoAfectado.classList.toggle('nuevo-estilo');
+botonCambiarEstilo.addEventListener('click', function () {
+    const color = '#' + Math.floor(Math.random()*16777215).toString(16);
+    botonCambiarEstilo.style.backgroundColor = color;
 });
